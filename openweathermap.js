55c9054be287868bb781e298ec2556c9ba6d2fc4
explosio <pavelsotnik92@gmail.com>
2019-03-02 17:33:03 +0200

    ...

--- openweathermap.js
+++ openweathermap.js
@@ -1,7 +1,7 @@
 
 var api_key = '4c074f065b78312844a57933e3e798a6';
 $.get('https://api.openweathermap.org/data/2.5/weather?id=703448&appid='+api_key, function (res){
-    document.img.src = 'https://psotnik92.github.io/explo/images/0_sun.gif';
+    //document.img.src = 'https://psotnik92.github.io/explo/images/0_sun.gif';
     var temp2 = (res.main.temp - 273.15).toFixed(1)
     document.querySelector('#text1').innerHTML = 'openweathermap.org'
     document.querySelector('#text2').innerHTML = res.name
