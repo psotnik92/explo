3e9341423f84fa8b1476f349292a75f329ae85b5
explosio <pavelsotnik92@gmail.com>
2019-03-08 15:26:37 +0200

    ...

--- openweathermap.js
+++ openweathermap.js
@@ -1,12 +1,19 @@
+navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var api_key = '4c074f065b78312844a57933e3e798a6';
 
-var api_key = '4c074f065b78312844a57933e3e798a6';
-$.get('https://api.openweathermap.org/data/2.5/weather?id=703448&appid='+api_key, function (res){
-    //document.img.src = 'https://psotnik92.github.io/explo/images/0_sun.gif';
-    var temp2 = (res.main.temp - 273.15).toFixed(1)
-    document.querySelector('#text1').innerHTML = 'openweather'
-    document.querySelector('#text2').innerHTML = res.name
-    document.querySelector('#text3').innerHTML = res.weather[0].description
-    document.querySelector('#text4').innerHTML = temp2+'°С'
-    document.querySelector('#text5').innerHTML = res.wind.speed+' m/sec'
+    $.get('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon +'&appid='+api_key, function (res){
+        var temp2 = (res.main.temp - 273.15).toFixed(1)
+        var dt2 = new Date(res.dt*1000).toLocaleString();
+        document.querySelector('#text1').innerHTML = 'openweather'
+        document.querySelector('#text2').innerHTML = res.weather[0].description
+        document.querySelector('#text3').innerHTML = temp2+'°С'
+        document.querySelector('#text4').innerHTML = res.wind.speed+' m/sec'
+        document.querySelector('#City').innerHTML = res.sys.country +"&nbsp"+ res.name
+        document.querySelector('#Time').innerHTML = dt2
+
+    })
 })
+
 
